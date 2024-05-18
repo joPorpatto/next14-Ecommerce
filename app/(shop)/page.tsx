70943a9f8ccdaaf231af7dd85e7db8406502884c
joPorpatto <joel.porpatto@gmail.com>
2024-05-18 17:49:32 -0300

    Se agregó el sidebar

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,10 +1,15 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components";
+import { ProductGrid } from "@/components/products/product-grid/ProductGrid";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
-    <main>
-      <h1>HOLA VIEJO</h1>
-      <h1 className={titleFont.className}>HOLA VIEJO</h1>
-    </main>
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+
+      <ProductGrid products={products} />
+    </>
   );
 }
